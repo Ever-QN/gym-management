7e189b6524a59c0caf81b90eb3939475d9e121c4
Quynh Ngo <QuynhTNgo@live.com>
2023-04-19 13:06:31 -0600

    Add 9 staff attributes in Data/StaffInfo.cs

--- GymApplication/Data/StaffInfo.cs
+++ GymApplication/Data/StaffInfo.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SQLite;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,15 @@ namespace GymApplication.Data
 {
     public class StaffInfo
     {
+        [PrimaryKey, AutoIncrement]
+        public int StaffId { get; set; }
+        public string StaffFirstName { get; set; }
+        public string StaffLastName { get; set; }
+        public string StaffEmail { get; set; }
+        public string StaffPhoneNumber { get; set; }
+        public string StaffHomeAddress { get; set; }
+        public string StaffPosition { get; set; }
+        public string StaffHireDate { get; set; }
+        public double StaffSalary { get; set; }
     }
 }
