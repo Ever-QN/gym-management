da9eb988f8f50516c0be2d04f4f5fe8b80e4cc7f
Quynh Ngo <QuynhTNgo@live.com>
2023-04-18 21:03:50 -0600

    Create Login.cs

---
+++ gym-application/Models/Login.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gym_application.Models
+{
+	public class Login
+	{
+	}
+}
