a4f2acda1ef434a82701cebe61fdfc00bf9de596
Quynh Ngo <QuynhTNgo@live.com>
2023-04-20 20:00:48 -0600

    Fill AdminInfo.cs with information

--- GymApplication/Data/AdminInfo.cs
+++ GymApplication/Data/AdminInfo.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SQLite;
 
 namespace GymApplication.Data
 {
     public class AdminInfo
     {
+        [PrimaryKey, AutoIncrement]
+        public int AdminId { get; set; }
+        public string AdminEmail { get; set; }
+        public string AdminPassword { get; set; }
     }
 }
