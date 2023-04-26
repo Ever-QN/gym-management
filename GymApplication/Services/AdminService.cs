89c558e2509f9bf6c31a0952d5683b9caaf59617
Quynh Ngo <QuynhTNgo@live.com>
2023-04-26 04:39:54 -0600

    Change local hardcoded validation to DB validation

--- GymApplication/Services/AdminService.cs
+++ GymApplication/Services/AdminService.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using GymApplication.Data;
+using Microsoft.AspNetCore.Components;
 using SQLite;
 
 namespace GymApplication.Services
