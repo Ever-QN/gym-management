3de43d5fdd378a45284b195874af43739493bf73
DiyaSharma15 <harry.diya161@gmail.com>
2023-04-27 16:52:52 -0600

    changes

--- GymApplication/MauiProgram.cs
+++ GymApplication/MauiProgram.cs
@@ -26,6 +26,8 @@ public static class MauiProgram
 
 		var adminDBPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), @"AdminDB.db3");
 		builder.Services.AddSingleton<AdminService>(x => ActivatorUtilities.CreateInstance<AdminService>(x, adminDBPath));
-        return builder.Build();
+		//var staffDBPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), @"StaffDB.db3");
+		//builder.Services.AddSingleton<StaffService>(x => ActivatorUtilities.CreateInstance<StaffService>(x, staffDBPath));
+		return builder.Build();
 	}
 }
