e541cb47d16e5aaaedb10a9c0bf16947d6e170ed
jonahscott <119908106+jonahscott@users.noreply.github.com>
2023-04-27 14:44:11 -0700

    Update BillingInfo.cs

--- GymApplication/Data/BillingInfo.cs
+++ GymApplication/Data/BillingInfo.cs
@@ -3,10 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace GymApplication.Data
 {
     public class BillingInfo
     {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string CustomerId { get; set; }
+
+        [Required]
+        public decimal Amount { get; set; }
+
+        [Required]
+        public DateTime InvoiceDate { get; set; }
     }
 }
+
